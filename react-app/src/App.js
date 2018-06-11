ca359828c9b0ea85db781e27c21d42a8df0ce955
Caleb Smith <32422676+CalebSmith0801@users.noreply.github.com>
2018-06-11 10:57:37 -0500

    CSS Modules Conditional Styles

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import StyleClasses from './App.css';
+import classes from './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -15,18 +15,10 @@ class App extends Component {
   
   
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-    };
-
     let persons = null;
+    let btnClass = '';
+
     if (this.state.showPersons){
-      style.backgroundColor = "red";
       persons = (
         <div>
           {this.state.persons.map((person, index) => { 
@@ -39,20 +31,22 @@ class App extends Component {
           })}
         </div>
       );
+
+      btnClass = classes.red;
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2)
-      classes.push( StyleClasses.red );
+      assignedClasses.push( classes.red );
     if (this.state.persons.length <= 1)
-      classes.push( StyleClasses.bold );
+      assignedClasses.push( classes.bold );
 
 
     return (
-      <div className={StyleClasses.App}>
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
